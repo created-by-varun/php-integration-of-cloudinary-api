d5a79cc84c08c2b4ce1a998c4a2a8c0549be8480
Varun V <varunvirupaksha321@gmail.com>
2020-01-14 17:32:20 +0530

    minor changes in layout

--- index.php
+++ index.php
@@ -11,32 +11,34 @@ Cloudinary::config(array("cloud_name" => "x", "api_key" => "y", "api_secret" => 
 
 
 <!DOCTYPE HTML>
-  <html>
-    <head>
-    	<title>Image Upload</title>
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-  	</head>
+<html>
 
-  <body>
+<head>
+	<title>Image Upload</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+	<link rel="stylesheet" href="style.css">
+</head>
 
-<form method="post" enctype="multipart/form-data">
-    Select image to upload:
-    <input type="file" name="fileToUpload" id="fileToUpload">
-    <div class="col-md-4">
-    	<input type="submit" value="Upload Image" name="submit">
-	</div>
-</form>
-
-<div>
-	<?php
+<body>
+	<form method="post" enctype="multipart/form-data">
+		<h2>
+			Select image to upload:
+		</h2>
+		<input type="file" name="fileToUpload" id="fileToUpload">
+		<div class="col-md-4"> <br>
+			<input type="submit" value="Upload" name="submit">
+		</div>
+	</form>
+	
+	<div>
+		<?php
 		if (isset($_POST["submit"]))
 			{
     			$cloudUpload = \Cloudinary\Uploader::upload($_FILES["fileToUpload"]['tmp_name']);
     			print_r($cloudUpload['url']);
 			}
 	?>
-</div>
-
-  </body>
+	</div>
+</body>
 
 </html>
